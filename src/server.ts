36f6c4b8d66dbcca00cc7b048314d2ca1ea40096
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2023-12-22 21:04:06 +0600

    Eslint,Prettier, Husky and Lint-staged setup

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/config'
 
 async function boostrap() {
   try {
-    await mongoose.connect(config.dbUrl as string);
-    console.log("Database connection successfull 🎁");
+    await mongoose.connect(config.dbUrl as string)
+    console.log('Database connection successfull 🎁')
 
     app.listen(config.port, () => {
-      console.log(`This server is listening port: ${config.port} 🏃`);
-    });
+      console.log(`This server is listening port: ${config.port} 🏃`)
+    })
   } catch (error) {
-    console.log("Database Connection Fail: ", error);
+    console.log('Database Connection Fail: ', error)
   }
 }
 
-boostrap();
+boostrap()
