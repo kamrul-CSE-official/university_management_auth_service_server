6a290152e97cc68fe7db5613ae628cc740caa797
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2023-12-22 13:08:35 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working Successfully...");
-});
+const kk = fkd
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working Successfully...')
+  const po = 12
+})
 
-export default app;
+export default app
