712b077c8e892ee7ef4022b9959f89451fc3ce63
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2023-12-22 22:36:13 +0600

    router created from created users

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
+import usersRoutes from './App/modules/users/users.route'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// application routes
+app.use('/api/v1/users/', usersRoutes)
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working successfully....')
+})
 
 export default app
